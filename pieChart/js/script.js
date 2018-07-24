e4c3dbccacf4ddea84c9a6bca9038815a8d21609
Catherine Clague <845870941@bun2-206-17.local>
2018-07-25 11:30:12 +1200

    MVP Pie Chart, dynamic

--- pieChart/js/script.js
+++ pieChart/js/script.js
@@ -1,4 +1,38 @@
+// our data
+const studentData = [
+	{
+		food: 'cake',
+		count: 6
+	},
+	{
+		food: 'pie',
+		count: 5
+	}
+];
+
 google.charts.load('current', { packages: ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {}
+// create chart
+function drawChart() {
+	const data = new google.visualization.DataTable();
+	// add columns
+	data.addColumn('string', 'Food');
+	data.addColumn('number', 'Count');
+
+	// create rows
+	for (var i = 0; i < studentData.length; i++) {
+		data.addRow([studentData[i].food, studentData[i].count]);
+	}
+
+	// style options
+	const options = {
+		title: 'Cake vs Pie'
+	};
+
+	// create chart
+	const chart = new google.visualization.PieChart(
+		document.getElementById('chartContainer')
+	);
+	chart.draw(data, options);
+}
