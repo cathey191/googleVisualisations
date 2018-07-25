c9d17459f12fd361c74712005d569292afaf8c7e
Catherine Clague <845870941@bun2-206-17.local>
2018-07-26 09:15:45 +1200

    Bubble student data

--- bubbleChart/js/script.js
+++ bubbleChart/js/script.js
@@ -3,12 +3,25 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
 	// creating data
+	// const data = google.visualization.arrayToDataTable([
+	// 	['Year', 'Birth', 'Deaths', 'Years', 'Marrages'],
+	// 	['2013', 58719, 29568, '2013', 19237],
+	// 	['2014', 57243, 31062, '2014', 20125],
+	// 	['2015', 61038, 31608, '2015', 19947],
+	// 	['2016', 59430, 31179, '2016', 20235]
+	// ]);
+
 	const data = google.visualization.arrayToDataTable([
-		['Year', 'Birth', 'Deaths', 'Years', 'Marrages'],
-		['2013', 58719, 29568, '2013', 19237],
-		['2014', 57243, 31062, '2014', 20125],
-		['2015', 61038, 31608, '2015', 19947],
-		['2016', 59430, 31179, '2016', 20235]
+		['Student', 'Module 1 Mark', 'Attendance %', 'To Be'],
+		['001', 87, 85, 'Designer'],
+		['002', 66, 69, 'Developer'],
+		['003', 64, 75, 'Designer'],
+		['004', 70, 60, 'Front End Developer'],
+		['005', 68, 80, 'Front End Developer'],
+		['006', 81, 80, 'Graphic Designer'],
+		['007', 90, 75, 'Web Developer'],
+		['008', 77, 60, 'Graphic Designer'],
+		['009', 86, 72, 'Web Developer']
 	]);
 
 	// styling chart
@@ -21,10 +34,10 @@ function drawChart() {
 		title: 'Births, Deaths & Marrages from NZ',
 		subtittle: 'From 2013 till 2016',
 		hAxis: {
-			title: 'Births'
+			title: 'Module 1 Mark'
 		},
 		vAxis: {
-			title: 'Deaths'
+			title: 'Attendance'
 		}
 	};
 
