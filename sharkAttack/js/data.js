eaeefb49659ddfdf0791c1c095d5f0059e7fd583
Catherine Clague <845870941@bun2-206-17.local>
2018-07-27 11:27:56 +1200

    Shark, long code to show three charts

--- sharkAttack/js/data.js
+++ sharkAttack/js/data.js
@@ -1,4 +1,4 @@
-const sharkData = [
+const countryData = [
 	['Country', 'Provoked', 'Unprovoked', 'Unknown', 'Questionable'],
 	['Australia', 2, 12, 2, 2],
 	['Brazil', 0, 2, 2, 0],
@@ -29,4 +29,29 @@ const ageData = [
 	['Unprovoked', 15, 5, 3, 8, 7, 0, 8],
 	['Unknown', 2, 1, 5, 1, 1, 0, 0],
 	['Questionable', 2, 0, 0, 0, 0, 0, 0]
+];
+
+const speciesData = [
+	[
+		'Species',
+		'Surfing',
+		'Diving/Snorkelling',
+		'Fishing',
+		'Swimming',
+		'Wading',
+		'Feeding Sharks',
+		'Watersports'
+	],
+	['Blacktip', 0, 1, 0, 0, 0, 0, 0],
+	['Bull', 0, 2, 2, 1, 2, 1, 0],
+	['Galapagos', 0, 1, 0, 0, 0, 0, 0],
+	['Grey Reef', 0, 1, 0, 0, 0, 1, 0],
+	['White', 2, 1, 0, 2, 0, 1, 1],
+	['Lemon', 0, 0, 1, 0, 0, 0, 0],
+	['Nurse', 0, 0, 1, 0, 0, 1, 0],
+	['Wobblegong', 1, 0, 0, 2, 0, 0, 0],
+	['Salmon', 0, 0, 0, 1, 0, 0, 0],
+	['Tiger', 1, 2, 1, 1, 0, 0, 1],
+	['Unidentified Species', 4, 0, 1, 1, 0, 0, 2],
+	['Not a Shark', 0, 1, 1, 0, 0, 1, 0]
 ];
--- sharkAttack/js/script.js
+++ sharkAttack/js/script.js
@@ -1,12 +1,22 @@
-google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+// get resorces
+google.charts.load('current', { packages: ['corechart', 'controls'] });
+google.charts.setOnLoadCallback(drawDashboard);
 
-function drawChart() {
+// run other functions
+function drawDashboard() {
+	var dashboard = new google.visualization.Dashboard(
+		document.getElementById('dashboardDiv')
+	);
+
+	drawCountry();
+	drawAge();
+	drawSpecies();
+}
+
+// draw country chart
+function drawCountry() {
 	// creating data
-	const data = google.visualization.arrayToDataTable(sharkData);
-
-	var female = 0;
-	var male = 1;
+	const data = google.visualization.arrayToDataTable(countryData);
 
 	// styling chart
 	const options = {
@@ -22,7 +32,59 @@ function drawChart() {
 
 	// setting chart to a bar chart
 	const chart = new google.visualization.ColumnChart(
-		document.getElementById('countryContainer')
+		document.getElementById('countryChart')
+	);
+
+	// creating chart
+	chart.draw(data, options);
+}
+
+// draw country chart
+function drawAge() {
+	// creating data
+	const data = google.visualization.arrayToDataTable(ageData);
+
+	// styling chart
+	const options = {
+		title: 'Type of Attacks Per Country',
+		hAxis: {
+			title: 'Country'
+		},
+		vAxis: {
+			title: 'Number'
+		},
+		isStacked: true
+	};
+
+	// setting chart to a bar chart
+	const chart = new google.visualization.ColumnChart(
+		document.getElementById('ageChart')
+	);
+
+	// creating chart
+	chart.draw(data, options);
+}
+
+// draw country chart
+function drawSpecies() {
+	// creating data
+	const data = google.visualization.arrayToDataTable(speciesData);
+
+	// styling chart
+	const options = {
+		title: 'Type of Attacks Per Country',
+		hAxis: {
+			title: 'Country'
+		},
+		vAxis: {
+			title: 'Number'
+		},
+		isStacked: true
+	};
+
+	// setting chart to a bar chart
+	const chart = new google.visualization.ColumnChart(
+		document.getElementById('speciesChart')
 	);
 
 	// creating chart
